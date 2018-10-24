ca6d0ea724adcdfee21b72f8d63bd9b2a80a8ec5
Tamara Koltsova <tamarinwm29@gmail.com>
2018-10-24 21:26:48 +0300

    first commit

--- sundrums/urls.py
+++ sundrums/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'^admin/$', admin.site.urls),
     url(r'^', include('sundrums_admin.urls')),
     path(r'robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'),name='robots.txt'),
     url(r'^tinymce/', include('tinymce.urls')),
