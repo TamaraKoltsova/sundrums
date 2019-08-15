78f3ae3db7f2fea822db1934a44bb36b7ade448b
Tamara Koltsova <tamarinwm29@gmail.com>
2019-08-15 21:47:11 +0300

    first commit

--- sundrums_admin/views.py
+++ sundrums_admin/views.py
@@ -57,8 +57,8 @@ def one_post_simple(request, slug):
     #id_for_categorie = Posts.objects.get(slug = slug)
     
     Post_one = get_object_or_404(Posts, slug=slug)
-    print(  "номер такой"   ,Post_one.categories_id)
-    name_for_categories= Post_categories.objects.get( id = int(Post_one.categories_3))
+    #print(  "номер такой"   ,Post_one.categories_id)
+    name_for_categories= Post_categories.objects.get( id = int(Post_one.categories_id))
     socbuttons = socbutton.objects.filter(is_active=True)
     tipe_kurss = tipe_kurs.objects.filter(is_active=True)
     return render(request, 'one_post_simple.html', locals())#
