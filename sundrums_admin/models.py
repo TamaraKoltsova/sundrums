3efc0335204a423f89b89ea033aae94ae0b4b892
Tamara Koltsova <tamarinwm29@gmail.com>
2019-08-24 19:38:20 +0300

    first commit

--- sundrums_admin/models.py
+++ sundrums_admin/models.py
@@ -95,7 +95,9 @@ class Posts(models.Model):
         
       def get_absolute_url(self):
         #return reverse('posts',args = [str(self.id)])
-        return reverse(self.categories__slug,args = [str(self.slug)])
+        slug_for_return = Post_categories.objects.get(slug = self.categories)
+        
+        return reverse('slug_for_return.slug',args = [str(self.slug)])
         #'''
 class teacher(models.Model):
       name = models.CharField(      max_length = 64, blank=True,   null=True, default= ' ', verbose_name= 'ФИО ' )
