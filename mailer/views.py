53c11a17383b698650b18c36dfd395b89ad45816
TamaraKoltsova <36993035+TamaraKoltsova@users.noreply.github.com>
2019-09-17 14:52:56 +0300

    Update views.py

--- mailer/views.py
+++ mailer/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.conf import settings
 from .decorators import check_recaptcha
@@ -12,11 +12,11 @@ from django.core.mail import send_mail
 def send_product(request):
     if request.recaptcha_is_valid:
         msg = ' %s \n    %s \n %s \n %s \n  %s \n  %s \n '%(request.POST['url'], request.POST['name'], request.POST['tel'], request.POST['email'], request.POST['topic'], request.POST['sms'])
-        send_mail(u'Вам тестовое сообщение с сайта sundrums.ru', msg , settings.EMAIL_HOST_USER, ['Sundrums@mail.ru'], fail_silently=False)
+        send_mail(u'Вам сообщение с сайта sundrums.ru', msg , settings.EMAIL_HOST_USER, ['Sundrums@mail.ru'], fail_silently=False)
     else:
         return HttpResponse(u'Пожайлуста пройдите проверку reCAPTCHA')        
-    return HttpResponse(u'Ваша заявка была отправлена')
-    
+    #return HttpResponse(u'Ваша заявка была отправлена')
+    return redirect("/")
 ''''def send_product_spec(request):
     product = Product.objects.get(id=request.POST['product_id'])
     msg = 'Заявочка на продукт с адреса %s \n Имя: %s \n Телефон: %s \n Продукт: %s \n  в комплектации: %s \n  по цене %s'%(request.POST['url'], request.POST['first_name'], request.POST['phone'], product.name, request.POST['product_complect'], request.POST['product_complect_price'] )
