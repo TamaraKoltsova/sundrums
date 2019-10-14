18e4566ad503e3b256a90dd469add82307b7c914
TamaraKoltsova <36993035+TamaraKoltsova@users.noreply.github.com>
2019-10-14 16:50:06 +0300

    Update urls.py

--- sundrums_admin/urls.py
+++ sundrums_admin/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
    url(r'^one_post/(?P<post_id>\w+)/', views.one_categories, name='one_post'),
-   url(r'^tipe_posts/(?P<post_id>\w+)/', views.tipe_posts, name='tipe_posts'),
+   #url(r'^tipe_posts/(?P<post_id>\w+)/', views.tipe_posts, name='tipe_posts'),
    
    
     
